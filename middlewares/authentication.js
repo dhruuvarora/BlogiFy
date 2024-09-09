dc4f770e8e8cdef32be05c55d2626ed81bcf2418
Dhruv Arora <121032965+dhruuvarora@users.noreply.github.com>
2024-09-09 12:07:32 +0530

    Add files via upload

---
+++ middlewares/authentication.js
@@ -0,0 +1,26 @@
+// middlewares/authentication.js
+
+const { validateToken } = require("../services/authentication");
+
+function checkForAuthenticationCookie(cookieName) {
+  return (req, res, next) => {
+    const tokenCookieValue = req.cookies[cookieName];
+    if (!tokenCookieValue) {
+      return next();
+    }
+
+    try {
+      const userPayload = validateToken(tokenCookieValue);
+      req.user = userPayload;
+    } catch (error) {
+      console.error("Token validation error:", error);
+      // Optionally handle the error
+    }
+
+    return next();
+  };
+}
+
+module.exports = {
+  checkForAuthenticationCookie,
+};
